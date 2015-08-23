1ef781c5c943b20887baff07d1b298ca6b01e03e
safwathannadeif <safwatdeif@gmail.com>
2015-08-23 17:12:08 -0400

    chg import files

--- CliWSWS/src/main/java/com/shdp/websockets/webservice/usecases/cli/SparkServer.java
+++ CliWSWS/src/main/java/com/shdp/websockets/webservice/usecases/cli/SparkServer.java
@@ -10,7 +10,7 @@ public class SparkServer    {
 	//	String servURL = "ws://localhost:8080/websockets-ws/websocketPathWS1" ; //test connect 
 	//	String servURL = "ws://localhost:8080/websockets-ws/test1.html" ;       //test webService 
 	//  String bigFileURL = "ws://localhost:8080/websockets-ws/websocketRtrvFileWS" ; //BigFileTesting End Pt
-	// 
+	// TomCat port 9080
 	//Cli: http://localhost:8888/#/ ,  http://localhost:8888/wsws/LisAvlWSWS
 	private static final String SPARK_IP_ADDRESS =  "localhost";
     private static final int SPARK_PORT =  8888;
@@ -19,8 +19,8 @@ public class SparkServer    {
 		//Setup Spark and start the Server 
 		SparkBase.port(SPARK_PORT);
 		SparkBase.ipAddress( SPARK_IP_ADDRESS ); 
-        SparkBase.staticFileLocation("/public");
-        //Setting the URI Active Resources for the Spark Server
+        SparkBase.staticFileLocation("/public");   
+        //Setting the URI Active Resources for the Spark Server  
         new WSWSResource() ;
 	}
 }
